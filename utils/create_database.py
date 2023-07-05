2164e4334e83e8830ebd85c43273054a631627d0
Fryskin <gman358325@gmail.com>
2023-07-05 16:25:17 +0200

    Func for database creating.

---
+++ utils/create_database.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+
+def create_database():
+    connection = psycopg2.connect(host='localhost', database='postgres',  user='postgres', password='bubacock')
+    connection.autocommit = True
+    cursor = connection.cursor()
+
+    cursor.execute('CREATE DATABASE "head_hunter_jobs"'
+                   ' WITH'
+                   ' OWNER = postgres'
+                   ' ENCODING = \'UTF8\''
+                   ' CONNECTION LIMIT = -1'
+                   ' IS_TEMPLATE = False;')
+
+    connection.commit()
+    connection.close()
+
